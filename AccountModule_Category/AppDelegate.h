d9b7d76eaaed5a1a19ce2aab6e897a32323610ff
sherlock.chan <>
2020-04-30 16:09:43 +0800

    first commit

---
+++ AccountModule_Category/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AccountModule_Category
+//
+//  Created by sherlock.chan on 2020/4/30.
+//  Copyright © 2020 sherlock.chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
